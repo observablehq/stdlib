5ca144d6c6a5ac4061e5b3da9bca0cab02cbd48d
Mike Bostock <mbostock@gmail.com>
2018-02-16 16:39:31 -0800

    Trim before rendering.

--- src/html.js
+++ src/html.js
@@ -2,7 +2,7 @@ import template from "./template";
 
 export default template(function(string) {
   var template = document.createElement("template");
-  template.innerHTML = string;
+  template.innerHTML = string.trim();
   var content = template.content;
   if (content.childNodes.length === 1) return content;
   var div = document.createElement("div");
--- src/svg.js
+++ src/svg.js
@@ -2,6 +2,6 @@ import template from "./template";
 
 export default template(function(string) {
   var root = document.createElementNS("http://www.w3.org/2000/svg", "g");
-  root.innerHTML = string;
+  root.innerHTML = string.trim();
   return root;
 });
