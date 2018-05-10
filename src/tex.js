5a5034cb6863a309ff25f5f626613ee15c5644e5
Mike Bostock <mbostock@gmail.com>
2018-05-10 13:38:41 -0700

    Resolve is now async.

--- src/tex.js
+++ src/tex.js
@@ -15,7 +15,7 @@ export default function(require, resource) {
   return function() {
     return Promise.all([
       require("katex@0.9.0/dist/katex.min.js"),
-      style(resource("katex@0.9.0/dist/katex.min.css"))
+      resource("katex@0.9.0/dist/katex.min.css").then(style)
     ]).then(function(values) {
       var katex = values[0], tex = renderer();
 
