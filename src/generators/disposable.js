849601a70e59769d8c9ecd75e8007f8dc91f3ef1
Mike Bostock <mbostock@gmail.com>
2018-06-04 12:42:57 -0700

    Make Generators.disposable iterable.

--- src/generators/disposable.js
+++ src/generators/disposable.js
@@ -1,6 +1,9 @@
+import that from "../that";
+
 export default function disposable(value, dispose) {
   let done = false;
   return {
+    [Symbol.iterator]: that,
     next: () => done ? {done: true} : (done = true, {done: false, value}),
     return: () => (done = true, dispose(value), {done: true}),
     throw: () => ({done: done = true})
