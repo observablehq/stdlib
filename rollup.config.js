a04626e2d20cdadedc0e5d5220095659765fd0eb
Mike Bostock <mbostock@gmail.com>
2018-12-11 12:59:28 -0800

    New bundle strategy.

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,24 @@
 import node from "rollup-plugin-node-resolve";
 import {terser} from "rollup-plugin-terser";
+import * as meta from "./package.json";
 
-const copyright = `// @observablehq/stdlib Copyright ${(new Date).getFullYear()} Observable, Inc.`;
+const copyright = `// @observablehq/stdlib v${meta.version} Copyright ${(new Date).getFullYear()} Observable, Inc.`;
 
-function config(output) {
-  return {
+export default [
+  {
     input: "src/index.js",
     plugins: [
       node(),
       terser({
-        toplevel: output.format === "es",
         output: {preamble: copyright},
         mangle: {reserved: ["RequireError"]}
       })
     ],
-    output
-  };
-}
-
-export default [
-  config({
-    format: "es",
-    file: "dist/stdlib.js"
-  }),
-  config({
-    format: "umd",
-    extend: true,
-    name: "observablehq",
-    file: "dist/stdlib.umd.js"
-  })
+    output: {
+      format: "umd",
+      extend: true,
+      name: "observablehq",
+      file: "dist/stdlib.js"
+    }
+  }
 ];
