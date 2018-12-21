7b5bf12303c7632cd1113011ae9b1f9be78e6b7b
Mike Bostock <mbostock@gmail.com>
2018-12-21 08:57:28 -0800

    Fix #75.

--- src/dom/download.js
+++ src/dom/download.js
@@ -1,11 +1,31 @@
-export default function(object, name, label) {
-  var a = document.createElement("a"),
-      b = a.appendChild(document.createElement("button"));
-  b.textContent = label == null ? "Download" : label;
-  a.download = name == null ? "untitled" : name;
-  a.onclick = function() {
-    var url = a.href = URL.createObjectURL(object);
-    setTimeout(function() { URL.revokeObjectURL(url); }, 50);
+export default function(value, name = "untitled", label = "Save") {
+  const a = document.createElement("a");
+  const b = a.appendChild(document.createElement("button"));
+  b.textContent = label;
+  a.download = name;
+
+  async function reset() {
+    await new Promise(requestAnimationFrame);
+    URL.revokeObjectURL(a.href);
+    a.removeAttribute("href");
+    b.textContent = label;
+    b.disabled = false;
+  }
+
+  a.onclick = async event => {
+    b.disabled = true;
+    if (a.href) return reset(); // Already saved.
+    b.textContent = "Saving…";
+    try {
+      const object = await (typeof value === "function" ? value() : value);
+      b.textContent = "Download";
+      a.href = URL.createObjectURL(object);
+    } catch (ignore) {
+      b.textContent = label;
+    }
+    if (event.eventPhase) return reset(); // Already downloaded.
+    b.disabled = false;
   };
+
   return a;
 }
