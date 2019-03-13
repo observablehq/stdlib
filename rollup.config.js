9775bd8486ccf130872de4b07dbf963f061fe424
Tom MacWright <tom@macwright.org>
2019-03-13 11:15:44 -0700

    Embed marked

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,21 @@
 import node from "rollup-plugin-node-resolve";
-import {terser} from "rollup-plugin-terser";
+import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
 import * as meta from "./package.json";
 
-const copyright = `// @observablehq/stdlib v${meta.version} Copyright ${(new Date).getFullYear()} Observable, Inc.`;
+const copyright = `// @observablehq/stdlib v${
+  meta.version
+} Copyright ${new Date().getFullYear()} Observable, Inc.`;
 
 export default [
   {
     input: "src/index.js",
     plugins: [
       node(),
+      commonjs(),
       terser({
-        output: {preamble: copyright},
-        mangle: {reserved: ["RequireError"]}
+        output: { preamble: copyright },
+        mangle: { reserved: ["RequireError"] }
       })
     ],
     output: {
--- src/md.js
+++ src/md.js
@@ -1,24 +1,23 @@
 import template from "./template";
+import marked from "marked";
 
 export default function(require) {
   return function() {
-    return require("marked@0.3.12/marked.min.js").then(function(marked) {
-      return template(function(string) {
-        var root = document.createElement("div");
-        root.innerHTML = marked(string, {langPrefix: ""}).trim();
-        var code = root.querySelectorAll("pre code[class]");
-        if (code.length > 0) {
-          require("@observablehq/highlight.js@1.1.1/highlight.min.js").then(function(hl) {
-            code.forEach(function (block) {
-              hl.highlightBlock(block);
-              block.parentNode.classList.add('observablehq--md-pre');
-            });
+    return template(function(string) {
+      var root = document.createElement("div");
+      root.innerHTML = marked(string, {langPrefix: ""}).trim();
+      var code = root.querySelectorAll("pre code[class]");
+      if (code.length > 0) {
+        require("@observablehq/highlight.js@1.1.1/highlight.min.js").then(function(hl) {
+          code.forEach(function (block) {
+            hl.highlightBlock(block);
+            block.parentNode.classList.add('observablehq--md-pre');
           });
-        }
-        return root;
-      }, function() {
-        return document.createElement("div");
-      });
+        });
+      }
+      return root;
+    }, function() {
+      return document.createElement("div");
     });
   };
 }
