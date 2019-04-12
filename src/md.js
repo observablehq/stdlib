4ba0582377395169a636ffa88b6e9d496a88bc02
Tom MacWright <tmcw@users.noreply.github.com>
2019-04-12 16:33:31 -0700

    Polyglot highlight.js support (#88)
    * Polyglot highlight.js support
    
    * Update to alpha.2 (AMD) and refine approach
    
    * Alpha 3
    
    * Reduce change: use .then instead of .finally, remove babel-eslint
    
    * Update to point to 2.0.0 highlight.js
    
    * Flip logic for positivity

--- src/md.js
+++ src/md.js
@@ -1,23 +1,46 @@
 import template from "./template";
 import marked from "marked";
 
+const HL_ROOT =
+  "https://cdn.jsdelivr.net/npm/@observablehq/highlight.js@2.0.0/";
+
 export default function(require) {
   return function() {
-    return template(function(string) {
-      var root = document.createElement("div");
-      root.innerHTML = marked(string, {langPrefix: ""}).trim();
-      var code = root.querySelectorAll("pre code[class]");
-      if (code.length > 0) {
-        require("@observablehq/highlight.js@1.1.1/highlight.min.js").then(function(hl) {
-          code.forEach(function (block) {
-            hl.highlightBlock(block);
-            block.parentNode.classList.add('observablehq--md-pre');
+    return template(
+      function(string) {
+        var root = document.createElement("div");
+        root.innerHTML = marked(string, { langPrefix: "" }).trim();
+        var code = root.querySelectorAll("pre code[class]");
+        if (code.length > 0) {
+          require(HL_ROOT + "highlight.min.js").then(function(hl) {
+            code.forEach(function(block) {
+              function done() {
+                hl.highlightBlock(block);
+                block.parentNode.classList.add("observablehq--md-pre");
+              }
+              if (hl.getLanguage(block.className)) {
+                done();
+              } else {
+                require(HL_ROOT + "async-languages/index.js")
+                  .then(index => {
+                    if (index.has(block.className)) {
+                      return require(HL_ROOT +
+                        "async-languages/" +
+                        index.get(block.className)).then(language => {
+                        hl.registerLanguage(block.className, language);
+                      });
+                    }
+                  })
+                  .then(done, done);
+              }
+            });
           });
-        });
+        }
+        return root;
+      },
+      function() {
+        return document.createElement("div");
       }
-      return root;
-    }, function() {
-      return document.createElement("div");
-    });
+    );
   };
 }
