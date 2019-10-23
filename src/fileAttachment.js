942218a1096534da205d427da23d521827546eb3
Mike Bostock <mbostock@gmail.com>
2019-10-23 14:32:26 -0700

    Update fileAttachment.js

--- src/fileAttachment.js
+++ src/fileAttachment.js
@@ -1,6 +1,6 @@
 async function remote_fetch(file) {
   const response = await fetch(await file.url());
-  if (!response.ok) throw new Error("Unable to load file");
+  if (!response.ok) throw new Error(`Unable to load file: ${file.name}`);
   return response;
 }
 
