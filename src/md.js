837957e4cccaa94f1e67d2818a06809c38318e27
Mike Bostock <mbostock@gmail.com>
2020-02-19 12:41:56 -0800

    Externalize marked.

--- src/md.js
+++ src/md.js
@@ -1,46 +1,47 @@
 import template from "./template.js";
-import marked from "marked";
 
 const HL_ROOT =
   "https://cdn.jsdelivr.net/npm/@observablehq/highlight.js@2.0.0/";
 
 export default function(require) {
   return function() {
-    return template(
-      function(string) {
-        var root = document.createElement("div");
-        root.innerHTML = marked(string, {langPrefix: ""}).trim();
-        var code = root.querySelectorAll("pre code[class]");
-        if (code.length > 0) {
-          require(HL_ROOT + "highlight.min.js").then(function(hl) {
-            code.forEach(function(block) {
-              function done() {
-                hl.highlightBlock(block);
-                block.parentNode.classList.add("observablehq--md-pre");
-              }
-              if (hl.getLanguage(block.className)) {
-                done();
-              } else {
-                require(HL_ROOT + "async-languages/index.js")
-                  .then(index => {
-                    if (index.has(block.className)) {
-                      return require(HL_ROOT +
-                        "async-languages/" +
-                        index.get(block.className)).then(language => {
-                        hl.registerLanguage(block.className, language);
-                      });
-                    }
-                  })
-                  .then(done, done);
-              }
+    return require("marked@0.3.12/marked.min.js").then(function(marked) {
+      return template(
+        function(string) {
+          var root = document.createElement("div");
+          root.innerHTML = marked(string, {langPrefix: ""}).trim();
+          var code = root.querySelectorAll("pre code[class]");
+          if (code.length > 0) {
+            require(HL_ROOT + "highlight.min.js").then(function(hl) {
+              code.forEach(function(block) {
+                function done() {
+                  hl.highlightBlock(block);
+                  block.parentNode.classList.add("observablehq--md-pre");
+                }
+                if (hl.getLanguage(block.className)) {
+                  done();
+                } else {
+                  require(HL_ROOT + "async-languages/index.js")
+                    .then(index => {
+                      if (index.has(block.className)) {
+                        return require(HL_ROOT +
+                          "async-languages/" +
+                          index.get(block.className)).then(language => {
+                          hl.registerLanguage(block.className, language);
+                        });
+                      }
+                    })
+                    .then(done, done);
+                }
+              });
             });
-          });
+          }
+          return root;
+        },
+        function() {
+          return document.createElement("div");
         }
-        return root;
-      },
-      function() {
-        return document.createElement("div");
-      }
-    );
+      );
+    });
   };
 }
