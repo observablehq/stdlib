5a954875443a8f4388fbbe8e469da920b3418194
Mike Bostock <mbostock@gmail.com>
2020-02-19 13:14:52 -0800

    Expose d3-require’s default resolve.

--- src/require.js
+++ src/require.js
@@ -1,5 +1,7 @@
 import {require as requireDefault, requireFrom} from "d3-require";
 
+export const resolve = requireDefault.resolve;
+
 export default function(resolve) {
   return resolve == null ? requireDefault : requireFrom(resolve);
 }
