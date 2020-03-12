563da0a7e279c4088ba26a570b7ccefea1d10497
Mike Bostock <mbostock@gmail.com>
2020-03-12 08:11:59 -0700

    For multiple select, expose an array of values.

--- src/generators/input.js
+++ src/generators/input.js
@@ -17,6 +17,7 @@ function valueof(input) {
     case "date": return input.valueAsDate;
     case "checkbox": return input.checked;
     case "file": return input.multiple ? input.files : input.files[0];
+    case "select-multiple": return Array.from(input.selectedOptions, o => o.value);
     default: return input.value;
   }
 }
