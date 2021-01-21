0502d361d676f67a61c2a0f06f9a77a0ecf24860
Mike Bostock <mbostock@gmail.com>
2021-01-20 16:23:33 -0800

    update dependencies

--- src/mutable.js
+++ src/mutable.js
@@ -4,7 +4,7 @@ export default function Mutable(value) {
   let change;
   Object.defineProperties(this, {
     generator: {value: observe(_ => void (change = _))},
-    value: {get: () => value, set: x => change(value = x)}
+    value: {get: () => value, set: x => change(value = x)} // eslint-disable-line no-setter-return
   });
   if (value !== undefined) change(value);
 }
