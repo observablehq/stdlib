7ff601df6f51f18bfce524a37d77c13df56b2546
Tom Larkworthy <tomlarkworthy@Toms-MacBook-Air.local>
2021-06-01 10:56:48 +0200

    Add promises.never as a readable alternative to returning an unresolved promise.
    I often return new Promise(() => {}) to hold up the dataflow, but this is ugly.
    Its not jsut me, see https://talk.observablehq.com/t/audio-worklet-example/5138
    So this is a more readable alternative that I think belongs with the Promises
    utilities.

--- src/promises/index.js
+++ src/promises/index.js
@@ -1,9 +1,11 @@
 import delay from "./delay.js";
+import never from "./never.js";
 import tick from "./tick.js";
 import when from "./when.js";
 
 export default {
   delay: delay,
+  never: never,
   tick: tick,
   when: when
 };
