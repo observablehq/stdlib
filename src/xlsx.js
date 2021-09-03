a8f79980de88403a680b7e7a22a323aaf68f10f6
Visnu Pitiyanuvath <visnup@gmail.com>
2021-09-03 16:29:18 -0700

    XLSX support with ExcelJS

---
+++ src/xlsx.js
@@ -0,0 +1,100 @@
+export class ExcelWorkbook {
+  constructor(workbook) {
+    Object.defineProperty(this, "_", {value: workbook});
+  }
+  sheetNames() {
+    return this._.worksheets.map(sheet => sheet.name);
+  }
+  sheet(name, options) {
+    name = (typeof name === "number") ? name = this.sheetNames()[name] : name + "";
+    const sheet = this._.getWorksheet(name);
+    if (!sheet) throw new Error(`Sheet not found: ${name}`);
+    return extract(sheet, options);
+  }
+}
+
+function extract(sheet, options = {}) {
+  const { range, headers = false } = options;
+  let [[c0, r0], [c1, r1]] = parseRange(range, sheet);
+  const empty = {};
+  const output = new Array(r1 - r0).fill(empty);
+
+  const headerRow = headers && sheet._rows[r0++];
+  const seen = new Set();
+  const names = [];
+  function name(n) {
+    if (!names[n]) {
+      let name = (headerRow ? valueOf(headerRow._cells[n]) : AA(n)) || AA(n);
+      while (seen.has(name)) name += "_";
+      seen.add((names[n] = name));
+    }
+    return names[n];
+  }
+
+  for (let r = r0; r < r1; r++) {
+    const _row = sheet._rows[r];
+    if (!_row || !_row.hasValues) continue;
+    const row = (output[r - r0] = {});
+    for (let c = c0; c < c1; c++) {
+      const value = valueOf(_row._cells[c]);
+      if (value) row[name(c)] = value;
+    }
+  }
+
+  output.columns = names.filter(() => true);
+  return output;
+}
+
+function valueOf(cell) {
+  if (!cell) return;
+  const { value } = cell;
+  if (value && typeof value === "object") {
+    if (value.formula) return value.result;
+    if (value.richText) return value.richText.map((d) => d.text).join("");
+    if (value.text && value.hyperlink)
+      return `<a href="${value.hyperlink}">${value.text}</a>`;
+  }
+  return value;
+}
+
+function parseRange(specifier = {}, { columnCount, rowCount }) {
+  if (typeof specifier === "string") {
+    const [
+      [c0 = 0, r0 = 0] = [],
+      [c1 = columnCount, r1 = rowCount] = []
+    ] = specifier.split(":").map(NN);
+    return [
+      [c0, r0],
+      [c1, r1]
+    ];
+  } else if (typeof specifier === "object") {
+    const {
+      start: [c0 = 0, r0 = 0] = [],
+      end: [c1 = columnCount, r1 = rowCount] = []
+    } = specifier;
+    return [
+      [c0, r0],
+      [c1, r1]
+    ];
+  }
+}
+
+function AA(x) {
+  let s = "";
+  x++;
+  do {
+    s = String.fromCharCode(64 + (x % 26 || 26)) + s;
+  } while ((x = Math.floor((x - 1) / 26)));
+  return s;
+}
+
+function NN(s = "") {
+  const [, sc, sr] = s.match(/^([a-zA-Z]+)?(\d+)?$/);
+  let c = undefined;
+  if (sc) {
+    c = 0;
+    for (let i = 0; i < sc.length; i++)
+      c += Math.pow(26, sc.length - i - 1) * (sc.charCodeAt(i) - 64);
+  }
+  return [c && c - 1, sr && +sr - 1];
+}
