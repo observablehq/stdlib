6f13d59bbc1b525efb33acb02ba056607afcbec5
Visnu Pitiyanuvath <visnup@gmail.com>
2021-09-14 10:51:03 -0700

    Simpler

--- src/xlsx.js
+++ src/xlsx.js
@@ -93,11 +93,9 @@ function toColumn(c) {
 // For example: "A1" -> [0, 0], "B2" -> [1, 1], "AA10" -> [26, 9].
 function fromCellReference(s) {
   const [, sc, sr] = s.match(/^([A-Z]*)(\d*)$/i);
-  let c = undefined;
-  if (sc) {
-    c = 0;
+  let c = 0;
+  if (sc)
     for (let i = 0; i < sc.length; i++)
       c += Math.pow(26, sc.length - i - 1) * (sc.charCodeAt(i) - 64);
-  }
   return [c ? c - 1 : undefined, sr ? +sr - 1 : undefined];
 }
