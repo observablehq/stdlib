c85c58158589727b9ea98a343b5e5deee805b030
Mike Bostock <mbostock@gmail.com>
2021-09-15 22:17:11 -0700

    Mbostock/xlsx tweaks (#254)
    * Update xlsx.js
    
    * Update xlsx.js
    
    * Use Object.create(null)
    
    * Prefer public ExcelJS APIs (#255)
    
    * Use Object.create(null)
    
    * Prefer public ExcelJS APIs
    
    * Use latest tap API
    
    * Coerce header row values to strings before fallback check
    
    * Update src/xlsx.js
    
    Co-authored-by: Mike Bostock <mbostock@gmail.com>
    
    * Public API
    
    Co-authored-by: Visnu Pitiyanuvath <visnup@gmail.com>

--- src/xlsx.js
+++ src/xlsx.js
@@ -21,12 +21,13 @@ export class Workbook {
   }
 }
 
-function extract(sheet, {range, headers = false} = {}) {
+function extract(sheet, {range, headers} = {}) {
   let [[c0, r0], [c1, r1]] = parseRange(range, sheet);
-  const headerRow = headers && sheet._rows[r0++];
+  const headerRow = headers ? sheet._rows[r0++] : null;
   let names = new Set(["#"]);
   for (let n = c0; n <= c1; n++) {
-    let name = (headerRow ? valueOf(headerRow._cells[n]) : null) || toColumn(n);
+    const value = headerRow ? valueOf(headerRow.findCell(n + 1)) : null;
+    let name = (value && value + "") || toColumn(n);
     while (names.has(name)) name += "_";
     names.add(name);
   }
@@ -34,13 +35,11 @@ function extract(sheet, {range, headers = false} = {}) {
 
   const output = new Array(r1 - r0 + 1);
   for (let r = r0; r <= r1; r++) {
-    const row = (output[r - r0] = Object.defineProperty({}, "#", {
-      value: r + 1,
-    }));
-    const _row = sheet._rows[r];
-    if (_row && _row.hasValues)
+    const row = (output[r - r0] = Object.create(null, {"#": {value: r + 1}}));
+    const _row = sheet.getRow(r + 1);
+    if (_row.hasValues)
       for (let c = c0; c <= c1; c++) {
-        const value = valueOf(_row._cells[c]);
+        const value = valueOf(_row.findCell(c + 1));
         if (value != null) row[names[c + 1]] = value;
       }
   }
@@ -52,14 +51,16 @@ function extract(sheet, {range, headers = false} = {}) {
 function valueOf(cell) {
   if (!cell) return;
   const {value} = cell;
-  if (value && value instanceof Date) return value;
-  if (value && typeof value === "object") {
-    if (value.formula || value.sharedFormula)
+  if (value && typeof value === "object" && !(value instanceof Date)) {
+    if (value.formula || value.sharedFormula) {
       return value.result && value.result.error ? NaN : value.result;
-    if (value.richText) return value.richText.map((d) => d.text).join("");
+    }
+    if (value.richText) {
+      return richText(value);
+    }
     if (value.text) {
       let {text} = value;
-      if (text.richText) text = text.richText.map((d) => d.text).join("");
+      if (text.richText) text = richText(text);
       return value.hyperlink && value.hyperlink !== text
         ? `${value.hyperlink} ${text}`
         : text;
@@ -67,6 +68,10 @@ function valueOf(cell) {
     return value;
   }
   return value;
+}
+
+function richText(value) {
+  return value.richText.map((d) => d.text).join("");
 }
 
 function parseRange(specifier = ":", {columnCount, rowCount}) {
