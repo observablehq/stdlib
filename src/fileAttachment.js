a32d1be152a6bbf6c45b5d17e844b170bc1af6b7
Cobus Theunissen <cobus@observablehq.com>
2021-10-17 21:13:12 -0700

    incorporated feedback

--- src/fileAttachment.js
+++ src/fileAttachment.js
@@ -42,8 +42,7 @@ export class AbstractFile {
   async tsv(options) {
     return dsv(this, "\t", options);
   }
-  async image(options={}) {
-    const {width, height} = options;
+  async image({width, height}={}) {
     const url = await this.url();
     return new Promise((resolve, reject) => {
       const i = new Image(width, height);
