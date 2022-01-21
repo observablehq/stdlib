195be68ddb1b12d52e5b61d9cf026cd8a4b9dd6e
Mike Bostock <mbostock@gmail.com>
2022-01-20 17:19:54 -0800

    update d3, plot, arquero

--- src/dependencies.js
+++ src/dependencies.js
@@ -1,7 +1,7 @@
 import dependency from "./dependency.js";
-export const d3 = dependency("d3", "7.2.1", "dist/d3.min.js");
+export const d3 = dependency("d3", "7.3.0", "dist/d3.min.js");
 export const inputs = dependency("@observablehq/inputs", "0.10.4", "dist/inputs.min.js");
-export const plot = dependency("@observablehq/plot", "0.3.2", "dist/plot.umd.min.js");
+export const plot = dependency("@observablehq/plot", "0.4.0", "dist/plot.umd.min.js");
 export const graphviz = dependency("@observablehq/graphviz", "0.2.1", "dist/graphviz.min.js");
 export const highlight = dependency("@observablehq/highlight.js", "2.0.0", "highlight.min.js");
 export const katex = dependency("@observablehq/katex", "0.11.1", "dist/katex.min.js");
@@ -14,6 +14,6 @@ export const vega = dependency("vega", "5.21.0", "build/vega.min.js");
 export const vegalite = dependency("vega-lite", "5.2.0", "build/vega-lite.min.js");
 export const vegaliteApi = dependency("vega-lite-api", "5.0.0", "build/vega-lite-api.min.js");
 export const arrow = dependency("apache-arrow", "4.0.1", "Arrow.es2015.min.js");
-export const arquero = dependency("arquero", "4.8.7", "dist/arquero.min.js");
+export const arquero = dependency("arquero", "4.8.8", "dist/arquero.min.js");
 export const topojson = dependency("topojson-client", "3.1.0", "dist/topojson-client.min.js");
 export const exceljs = dependency("exceljs", "4.3.0", "dist/exceljs.min.js");
