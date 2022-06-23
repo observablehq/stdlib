40d61935a074aef15efaa192e67d308d463e8b42
Mike Bostock <mbostock@gmail.com>
2022-06-23 10:05:59 -0400

    allow value interpolation

--- src/py.js
+++ src/py.js
@@ -2,10 +2,17 @@ import {pyodide as Pyodide} from "./dependencies.js";
 
 export default async function py(require) {
   const pyodide = await (await require(Pyodide.resolve())).loadPyodide();
-  return async function py() {
-    const code = String.raw.apply(String, arguments);
+  return async function py(strings, ...values) {
+    let globals = {};
+    const code = strings.reduce((code, string, i) => {
+      if (!(i in values)) return code + string;
+      const name = `_${i}`;
+      globals[name] = values[i];
+      return code + string + name;
+    }, "");
+    globals = pyodide.toPy(globals);
     await pyodide.loadPackagesFromImports(code);
-    const value = await pyodide.runPython(code);
+    const value = await pyodide.runPythonAsync(code, {globals});
     return pyodide.isPyProxy(value) ? value.toJs() : value;
   };
 }
