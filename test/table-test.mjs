76be5852e991c55310e4c32856fe3ce09b242b55
Mike Bostock <mbostock@gmail.com>
2022-10-11 17:59:19 -0700

    test against sort mutation

--- test/table-test.mjs
+++ test/table-test.mjs
@@ -274,6 +274,18 @@ describe("__table", () => {
     );
   });
 
+  it("__table sort does not mutate input", () => {
+    const operations = {...EMPTY_TABLE_DATA.operations, sort: [{column: "a", direction: "desc"}]};
+    assert.deepStrictEqual(
+      __table(source, operations),
+      [{a: 3, b: 6, c: 9}, {a: 2, b: 4, c: 6}, {a: 1, b: 2, c: 3}]
+    );
+    assert.deepStrictEqual(
+      source,
+      [{a: 1, b: 2, c: 3}, {a: 2, b: 4, c: 6}, {a: 3, b: 6, c: 9}]
+    );
+  });
+
   it("__table slice", () => {
     const operationsToNull = {...EMPTY_TABLE_DATA.operations, slice: {from: 1, to: null}};
     assert.deepStrictEqual(__table(source, operationsToNull), [{a: 2, b: 4, c: 6}, {a: 3, b: 6, c: 9}]);
