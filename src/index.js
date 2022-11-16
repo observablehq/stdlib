f8992191c59f6deb0dfd7a7bec6194f5d32c95b0
mkfreeman <michael.k.freeman@gmail.com>
2022-11-16 10:04:19 -0500

    Export __table function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 export {FileAttachments, AbstractFile} from "./fileAttachment.js";
 export {Library} from "./library.js";
 export {getArrowTableSchema, isArrowTable} from "./arrow.js";
-export {makeQueryTemplate, loadDataSource, arrayIsPrimitive, isDataArray, isDatabaseClient} from "./table.js";
+export {
+  makeQueryTemplate,
+  loadDataSource,
+  arrayIsPrimitive,
+  isDataArray,
+  isDatabaseClient,
+  __table
+} from "./table.js";
