c8c153cab92b9e14b54cecfd2782a3068dd7ff0a
Michael Freeman <michael.k.freeman@gmail.com>
2022-11-16 18:44:43 -0500

    Export __table function (#323)
    * Export __table function
    
    * Rename __table export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 export {FileAttachments, AbstractFile} from "./fileAttachment.js";
 export {Library} from "./library.js";
 export {getArrowTableSchema, isArrowTable} from "./arrow.js";
-export {makeQueryTemplate, loadDataSource, arrayIsPrimitive, isDataArray, isDatabaseClient} from "./table.js";
+export {
+  makeQueryTemplate,
+  loadDataSource,
+  arrayIsPrimitive,
+  isDataArray,
+  isDatabaseClient,
+  __table as applyDataTableOperations
+} from "./table.js";
