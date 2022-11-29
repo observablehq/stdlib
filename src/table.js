c7102196fe7f89691dc14264840f5cdffa82e16f
Libbey White <libbey@observablehq.com>
2022-11-29 12:44:41 -0800

    Possible fix

--- src/table.js
+++ src/table.js
@@ -177,9 +177,10 @@ function sourceCache(loadSource) {
   return (source, name) => {
     if (!source) throw new Error("data source not found");
     let promise = cache.get(source);
-    if (!promise) {
+    if (!promise || (isDataArray(source) && source.length !== promise._numRows)) {
       // Warning: do not await here! We need to populate the cache synchronously.
       promise = loadSource(source, name);
+      promise._numRows = source.length; // This will be undefined for DatabaseClients
       cache.set(source, promise);
     }
     return promise;
