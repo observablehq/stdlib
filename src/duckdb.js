9fa0629411ee3de35305d5afb63c51f36d992261
Libbey White <libbey@observablehq.com>
2022-12-02 10:02:47 -0800

    Add comment about stdlib and Arquero versions

--- src/duckdb.js
+++ src/duckdb.js
@@ -219,6 +219,8 @@ async function insertArrowTable(database, name, table, options) {
 }
 
 async function insertArqueroTable(database, name, source) {
+  // TODO When we have stdlib versioning and can upgrade Arquero to version 5,
+  // we can then call source.toArrow() directly, with insertArrowTable()
   const arrow = await loadArrow();
   const table = arrow.tableFromIPC(source.toArrowBuffer());
   return await insertArrowTable(database, name, table);
