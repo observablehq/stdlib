6acab1d6b1f350b338b774adcdcb0c91b3e8655d
Libbey White <libbey@observablehq.com>
2023-01-31 10:28:22 -0800

    Don't mutate schema

--- src/table.js
+++ src/table.js
@@ -633,8 +633,9 @@ export function __table(source, operations) {
     for (const {name, type} of operations.type) {
       types.set(name, type);
       // update schema with user-selected type
+      if (schema === input.schema) schema = schema.slice(); // copy on write
       const colIndex = schema.findIndex((col) => col.name === name);
-      if (colIndex > -1) schema[colIndex] = {name, type};
+      if (colIndex > -1) schema[colIndex] = {...schema[colIndex], type};
     }
     source = source.map(d => coerceRow(d, types));
   }
